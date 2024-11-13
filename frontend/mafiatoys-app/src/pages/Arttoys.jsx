385f347ffcc22d9399aa249de8d4c9a8e1d538c1
Thanachotelu <keakwanwong_t@silpakorn.edu>
2024-11-13 22:58:35 +0700

    fix shopdetail

--- frontend/mafiatoys-app/src/pages/Arttoys.jsx
+++ frontend/mafiatoys-app/src/pages/Arttoys.jsx
@@ -1,169 +1,128 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import TopMenu from '../components/TopMenu'; // ส่วนแสดงเมนูด้านบน
 import Footer from '../components/Footer'; // ส่วนแสดง Footer
+import BannerShop from '../components/BannerShop';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 import RangeSlider from 'react-range-slider-input';
 import 'react-range-slider-input/dist/style.css';
 import '../index.css';
 
-const products = [
-  { id: 1, name: "Art Toy 1", price: "฿1,200", imageUrl: "https://prod-eurasian-res.popmart.com/default/20240304_164111_296715__1200x1200.jpg?x-oss-process=image/resize,p_30,format,webp,format,webp", shop: "POP MART" },
-  { id: 2, name: "Art Toy 2", price: "฿1,500", imageUrl: "https://prod-eurasian-res.popmart.com/default/20240304_164111_296715__1200x1200.jpg?x-oss-process=image/resize,p_30,format,webp,format,webp", shop: "ATONGS SHOPP" },
-  { id: 3, name: "Art Toy 3", price: "฿2,000", imageUrl: "https://prod-eurasian-res.popmart.com/default/20240304_164111_296715__1200x1200.jpg?x-oss-process=image/resize,p_30,format,webp,format,webp", shop: "ART TOYS" },
-  { id: 4, name: "Art Toy 4", price: "฿2,500", imageUrl: "https://prod-eurasian-res.popmart.com/default/20240304_164111_296715__1200x1200.jpg?x-oss-process=image/resize,p_30,format,webp,format,webp", shop: "GACHABOX" },
-  { id: 5, name: "Art Toy 5", price: "฿3,000", imageUrl: "https://prod-eurasian-res.popmart.com/default/20240304_164111_296715__1200x1200.jpg?x-oss-process=image/resize,p_30,format,webp,format,webp", shop: "PIECE OF JOY" },
-  { id: 6, name: "Art Toy 6", price: "฿3,500", imageUrl: "https://prod-eurasian-res.popmart.com/default/20240304_164111_296715__1200x1200.jpg?x-oss-process=image/resize,p_30,format,webp,format,webp", shop: "POP MART" }
-];
-
 const Arttoys = () => {
-  const [priceRange, setPriceRange] = useState([0, 5000]);
+  const [products, setProducts] = useState([]);
+  const [productImages, setProductImages] = useState({});
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [priceRange, setPriceRange] = useState([0, 20000]);
   const [selectedCategories, setSelectedCategories] = useState([]);
-  const [selectedShops, setSelectedShops] = useState(["ART TOYS"]); // Default selected shop
   const [sortOption, setSortOption] = useState('');
+  const [selectedBrands, setSelectedBrands] = useState([]);
 
   const handlePriceRangeChange = (value) => {
     setPriceRange(value);
-  };
+};
 
-  const handleCategoryChange = (e) => {
+const handleSortChange = (e) => {
+    setSortOption(e.target.value);
+};
+
+// ดึงข้อมูลรูปภาพของสินค้าจาก API getProductImages
+const fetchProductImages = async (productId) => {
+    try {
+        const response = await axios.get(`http://localhost:8080/api/v1/products/images/${productId}`);
+        
+        // ตรวจสอบว่า data.images และ data.images[0] มีค่าหรือไม่
+        setProductImages(prevImages => ({
+            ...prevImages,
+            [productId]: response.data.images && response.data.images.length > 0 ? response.data.images[0]?.image_url : ''
+        }));
+    } catch (error) {
+        console.error('Error fetching product images:', error);
+    }
+};
+
+useEffect(() => {
+    // เมื่อได้ข้อมูลสินค้าแล้ว เราจะดึงข้อมูลภาพสำหรับแต่ละสินค้า
+    products.forEach(product => {
+        fetchProductImages(product.product_id);
+    });
+}, [products]);
+
+// ดึงข้อมูลสินค้าทั้งหมดจาก API products
+useEffect(() => {
+    const fetchProducts = async () => {
+        try {
+            const response = await axios.get('http://localhost:8080/api/v1/products');
+            setProducts(response.data.items || []);
+        } catch (err) {
+            setError('Failed to fetch products');
+        } finally {
+            setLoading(false);
+        }
+    };
+    fetchProducts();
+}, []);
+
+const handleCategoryChange = (e) => {
     const category = e.target.value;
     if (e.target.checked) {
-      setSelectedCategories([...selectedCategories, category]);
+        setSelectedCategories([...selectedCategories, category]);
     } else {
-      setSelectedCategories(selectedCategories.filter(item => item !== category));
+        setSelectedCategories(selectedCategories.filter(item => item !== category));
     }
-  };
+};
 
-  const handleShopChange = (e) => {
-    const shop = e.target.value;
+const handleBrandChange = (e) => {
+    const brand = e.target.value;
     if (e.target.checked) {
-      setSelectedShops([...selectedShops, shop]);
+        setSelectedBrands([...selectedBrands, brand]);
     } else {
-      setSelectedShops(selectedShops.filter(item => item !== shop));
+        setSelectedBrands(selectedBrands.filter(item => item !== brand));
     }
-  };
+};
 
-  const handleSortChange = (e) => {
-    setSortOption(e.target.value);
-  };
-
-  const filteredProducts = products.filter(product => {
-    const price = parseInt(product.price.replace("฿", "").replace(",", ""));
+// กรองสินค้าตามราคาและหมวดหมู่และร้านค้า
+const filteredProducts = products.filter(product => {
+    let price = 0;
+    if (typeof product.price === 'string') {
+        price = parseInt(product.price.replace("฿", "").replace(",", ""));
+    } else if (typeof product.price === 'number') {
+        price = product.price;
+    }
     const isInPriceRange = price >= priceRange[0] && price <= priceRange[1];
-    const isInCategory = selectedCategories.length === 0 || selectedCategories.some(category => product.name.includes(category));
-    const isInShop = selectedShops.length === 0 || selectedShops.includes(product.shop);
-    return isInPriceRange && isInCategory && isInShop;
-  });
+    const isInCategory = selectedCategories.length === 0 || selectedCategories.includes(product.product_type);
+    const isInBrand = selectedBrands.length === 0 || selectedBrands.includes(product.brand);
+    const isFromArtToysSeller = product.sellers[0]?.name === "Art Toys";
+    return isInPriceRange && isInCategory && isInBrand && isFromArtToysSeller;
+});
 
-  const sortedProducts = filteredProducts.sort((a, b) => {
+const sortedProducts = filteredProducts.sort((a, b) => {
     if (sortOption === 'price-asc') {
-      return parseInt(a.price.replace('฿', '').replace(',', '')) - parseInt(b.price.replace('฿', '').replace(',', ''));
+        return a.price - b.price;
     }
     if (sortOption === 'price-desc') {
-      return parseInt(b.price.replace('฿', '').replace(',', '')) - parseInt(a.price.replace('฿', '').replace(',', ''));
+        return b.price - a.price;
     }
     if (sortOption === 'name-asc') {
-      return a.name.localeCompare(b.name);
+        return a.name.localeCompare(b.name);
     }
     if (sortOption === 'name-desc') {
-      return b.name.localeCompare(a.name);
+        return b.name.localeCompare(a.name);
     }
     return 0;
-  });
-
-
+});
+if (loading) return <div>Loading...</div>;
+if (error) return <div>{error}</div>;
   return (
     <div>
       <TopMenu />
 
-      {/* ป้ายโฆษณาของร้านค้า */}
-      {/* เพิ่ม <style> ที่กำหนด keyframes animation */}
-      <style>
-        {`
-                    @keyframes marquee {
-                        0% {
-                            transform: translateX(100%); /* เริ่มต้นที่ขวาสุด */
-                        }
-                        100% {
-                            transform: translateX(-300%); /* เลื่อนจนถึงขวาสุด */
-                        }
-                    }
-                `}
-      </style>
-      {/* ป้ายโฆษณาของร้านค้า */}
-      <div className="advertisement-banner" style={{
-        backgroundColor: '#FFD700',
-        padding: '20px',
-        textAlign: 'center',
-        marginBottom: '20px',
-        fontSize: '1.25rem',
-        fontWeight: 'bold',
-        borderRadius: '10px',
-        boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)',
-        overflow: 'hidden', // ให้ขอบข้อความที่เลื่อนออกไปไม่แสดง
-        position: 'relative',
-        height: '70px'
-      }}>
-        <div style={{
-          display: 'inline-block',
-          whiteSpace: 'nowrap', // ให้ข้อความอยู่ในแถวเดียว
-          animation: 'marquee 11s linear infinite', // กำหนดความเร็วและการวนลูปของการเคลื่อนที่
-          position: 'absolute', // ใช้ position เพื่อให้ข้อความเคลื่อนไปจากขวาไปซ้าย
-          left: '100%' // เริ่มต้นจากขวาสุด
-        }}>
-          <p>🚨 Welcome to Art Toys! 🚨 Check out our latest art toys and limited edition figures! 🛍️</p>
-        </div>
-      </div>
-
-      {/* รูปภาพโฆษณาต่อจากป้ายโฆษณา */}
-
-      <div style={{
-        marginBottom: '20px',
-        textAlign: 'center'
-      }}>
-        <img
-          src="/assets/background_arttoys.png"
-          alt="Advertisement"
-          style={{
-            width: '100%',
-            height: 'auto',
-            borderRadius: '10px',
-            boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)'
-          }}
-        />
-      </div>
-
-      <div style={{
-        marginBottom: '20px',
-        textAlign: 'center',
-        padding: '0 10px' // เพิ่ม padding ด้านซ้ายและขวา เพื่อไม่ให้แนบติดขอบ
-      }}>
-        <img
-          src="https://scontent.fbkk8-2.fna.fbcdn.net/v/t51.75761-15/465973308_17984281331716691_3026839256678608436_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=127cfc&_nc_ohc=hh8KrduNJYAQ7kNvgFvdY6T&_nc_zt=23&_nc_ht=scontent.fbkk8-2.fna&_nc_gid=ASugIOvdLokWP6XXcrjau-f&oh=00_AYB1a9kwrxDzbDJCJiVzzNJ1oTCFRAgGfyHML_AwfitUjQ&oe=6735345F"
-          alt="Advertisement"
-          style={{
-            width: '50%', // กำหนดให้รูปภาพขยายเต็มความกว้าง
-            maxWidth: '1200px', // จำกัดขนาดความกว้างสูงสุด
-            height: 'auto',
-            borderRadius: '10px',
-            boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)',
-            margin: '0 auto' // จัดกลาง
-          }}
-        />
-
-        <img
-          src="https://scontent.fbkk13-3.fna.fbcdn.net/v/t51.75761-15/465811049_17984281358716691_7104167782880976641_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=127cfc&_nc_ohc=abTkkm-Lwh4Q7kNvgH8BMJc&_nc_zt=23&_nc_ht=scontent.fbkk13-3.fna&_nc_gid=ADRp1RRun5T_ZNwxOXYCdzV&oh=00_AYC_ojGgMbtMbjgR0Zs_FKvaxF_yoJ_Z7u2MFaWobL_A8A&oe=673557D8"
-          alt="Advertisement"
-          style={{
-            width: '50%',
-            maxWidth: '1200px', // จำกัดขนาดความกว้างสูงสุด
-            height: 'auto',
-            borderRadius: '10px',
-            boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)',
-            margin: '0 auto' // จัดกลาง
-          }}
-        />
-      </div>
+      <BannerShop storeName="Art Toys" backgroundColor="#FFD700" banner_img1="/assets/images/background_arttoys.png"
+                  banner_img2 = "https://scontent.fbkk8-2.fna.fbcdn.net/v/t51.75761-15/465973308_17984281331716691_3026839256678608436_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=127cfc&_nc_ohc=hh8KrduNJYAQ7kNvgFvdY6T&_nc_zt=23&_nc_ht=scontent.fbkk8-2.fna&_nc_gid=ASugIOvdLokWP6XXcrjau-f&oh=00_AYB1a9kwrxDzbDJCJiVzzNJ1oTCFRAgGfyHML_AwfitUjQ&oe=6735345F"
+                  banner_img3 = "https://scontent.fbkk13-3.fna.fbcdn.net/v/t51.75761-15/465811049_17984281358716691_7104167782880976641_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=127cfc&_nc_ohc=abTkkm-Lwh4Q7kNvgH8BMJc&_nc_zt=23&_nc_ht=scontent.fbkk13-3.fna&_nc_gid=ADRp1RRun5T_ZNwxOXYCdzV&oh=00_AYC_ojGgMbtMbjgR0Zs_FKvaxF_yoJ_Z7u2MFaWobL_A8A&oe=673557D8" 
+                  width_img2 = "50%" height_img2 = "50%" width_img3 = "50%" height_img3 = "50%"
+      />
 
 
       <div className="all-products-container">
@@ -173,7 +132,7 @@ const Arttoys = () => {
             <label>
               <input
                 type="checkbox"
-                value="Art Toy"
+                value="arttoy"
                 onChange={handleCategoryChange}
               />
               ㅤArt Toy
@@ -181,7 +140,7 @@ const Arttoys = () => {
             <label>
               <input
                 type="checkbox"
-                value="Figure"
+                value="figure"
                 onChange={handleCategoryChange}
               />
               ㅤFigure
@@ -189,67 +148,78 @@ const Arttoys = () => {
             <label>
               <input
                 type="checkbox"
-                value="Accessories"
+                value="accessory"
                 onChange={handleCategoryChange}
               />
               ㅤAccessories
             </label>
           </div>
 
-          <h2>Shop</h2>
-          <div className="category-checkbox">
-            <label>
-              <input
-                type="checkbox"
-                value="ATONGS SHOPP"
-                onChange={handleShopChange}
-                checked={selectedShops.includes("ATONGS SHOPP")}
-              />
-              ㅤATONGS SHOPP
-            </label>
-            <label>
-              <input
-                type="checkbox"
-                value="ART TOYS"
-                onChange={handleShopChange}
-                checked={selectedShops.includes("ART TOYS")}
-              />
-              ㅤART TOYS
-            </label>
-            <label>
-              <input
-                type="checkbox"
-                value="GACHABOX"
-                onChange={handleShopChange}
-                checked={selectedShops.includes("GACHABOX")}
-              />
-              ㅤGACHABOX
-            </label>
-            <label>
-              <input
-                type="checkbox"
-                value="POP MART"
-                onChange={handleShopChange}
-                checked={selectedShops.includes("POP MART")} // Default checked
-              />
-              ㅤPOP MART
-            </label>
-            <label>
-              <input
-                type="checkbox"
-                value="PIECE OF JOY"
-                onChange={handleShopChange}
-                checked={selectedShops.includes("PIECE OF JOY")}
-              />
-              ㅤPIECE OF JOY
-            </label>
-          </div>
+          <h2>Brand</h2>
+            <div className="category-checkbox">
+                <label>
+                    <input
+                        type="checkbox"
+                        value="52toys"
+                        onChange={handleBrandChange}
+                    />
+                    ㅤ52toys
+                </label>
+                <label>
+                    <input
+                        type="checkbox"
+                        value="Art Toys"
+                        onChange={handleBrandChange}
+                    />
+                    ㅤArt Toys
+                </label>
+                <label>
+                    <input
+                        type="checkbox"
+                        value="Duckyo’s"
+                        onChange={handleBrandChange}
+                    />
+                    ㅤDuckyo’s
+                </label>
+                <label>
+                    <input
+                        type="checkbox"
+                        value="FUFUTIETIE"
+                        onChange={handleBrandChange}
+                    />
+                    ㅤFUFUTIETIE
+                </label>
+                <label>
+                    <input
+                        type="checkbox"
+                        value="LABUBU"
+                        onChange={handleBrandChange}
+                    />
+                    ㅤLABUBU
+                </label>
+                <label>
+                    <input
+                        type="checkbox"
+                        value="Sanrio"
+                        onChange={handleBrandChange}
+                    />
+                    ㅤSanrio
+                </label>
+                <label>
+                    <input
+                        type="checkbox"
+                        value="THE MONSTERS"
+                        onChange={handleBrandChange}
+                    />
+                    ㅤTHE MONSTERS
+                </label>
+            </div>
 
           <div className="price-range-container">
             <div className="range-label">ช่วงราคา</div>
             <RangeSlider
               min={0}
-              max={5000}
+              max={20000}
               step={100}
               value={priceRange}
               onInput={handlePriceRangeChange}
@@ -274,18 +244,40 @@ const Arttoys = () => {
             </select>
           </div>
 
-          <h1 className="title">All Products</h1>
+          <h1 className="title">All Products[{sortedProducts.length}]</h1>
+          {sortedProducts.length.length === 0 && <p>No products found</p>}
           <div className="product-grid">
-            {sortedProducts.map(product => (
-              <div className="card" key={product.id}>
-                <img src={product.imageUrl} alt={product.name} className="product-image" />
-                <div className="container">
-                  <h6><b>{product.name}</b></h6>
-                  <p>{product.price}</p>
-                  <p><small>Shop: {product.shop}</small></p>
-                </div>
-              </div>
-            ))}
+            {sortedProducts.map(product => {
+                // ตรวจสอบว่า images มีค่าหรือไม่ และเป็น Array หรือไม่
+                const primaryImage = Array.isArray(product.images) && product.images.length > 0 
+                    ? product.images.find(img => img.is_primary && product.category_id === product.categories[0]?.id) 
+                    : null;
+
+                // ถ้าไม่มีรูปภาพที่เป็น primary ให้ใช้รูปแรกใน Array หรือเป็นค่าว่างถ้าไม่มีรูปใดๆ
+                const imageUrl = primaryImage 
+                    ? primaryImage.image_url 
+                    : (Array.isArray(product.images) && product.images[0]?.image_url) || '';
+
+                const displayedImageUrl = productImages[product.product_id] || imageUrl || '/placeholder.jpg';
+
+                return (
+                    <Link to={`/products/${product.product_id}`} style={{ textDecoration: 'none', color: 'inherit' }}>
+                        <div className="card" key={product.product_id} style={{ cursor: 'pointer' , height: '100%'}}>
+                            <img
+                                src={displayedImageUrl} 
+                                alt={product.name} 
+                                className="product-image" 
+                                onError={(e) => { e.target.src = '/placeholder.jpg'; }} // ใช้ placeholder ถ้าภาพโหลดไม่ได้
+                            />
+                            <div className="container">
+                                <h5 align="left"><b style={{color: 'gray'}}>{product.sellers[0]?.name || "Unknown Shop"}</b></h5>
+                                <h6 align="left"><b>{product.name}</b></h6>
+                                <p align="left"style={{ color: 'red', fontSize: '1.1em' }}>฿{product.price.toLocaleString()}.00</p>
+                            </div>
+                        </div>
+                    </Link>
+                );
+            })}
           </div>
         </div>
       </div>
